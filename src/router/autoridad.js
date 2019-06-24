cc6a660c0a71d60f5dc3b53cde45fb68a3655474
AleaIactaEst <maikholsozaki@gmail.com>
2019-06-24 20:33:54 -0300

    Actualizacion rutas sin terminar

--- src/router/autoridad.js
+++ src/router/autoridad.js
@@ -3,17 +3,18 @@ const path = require('path');
 const pool = require('../database');
 const {isLoggedIn, NotLoggedIn} = require('../autenticacion');
 
-//Ver tabla de autoridades
+//Enviar tabla autoridades
 router.get('/',isLoggedIn,async (req,res,next)=>{
     const autoridades = await pool.query('SELECT * FROM autoridades');
 });
 
-//ver autoridad
+//enviar autoridad con ID
 router.get('/:idAutoridad',isLoggedIn,async (req,res,next)=>{
     const {idAutoridad} = req.params;
     const autoridad = await pool.query('SELECT * FROM autoridades WHERE idAutoridad = ?',[idAutoridad]);
 });
 
+//Recibir actualizacion de autoridad con ID
 router.post('/update',isLoggedIn,async(req,res,next)=>{
     const {
         idAutoridad,
@@ -38,7 +39,36 @@ router.post('/update',isLoggedIn,async(req,res,next)=>{
         fechaNacimiento,
         FichaMedica
     }
-    await pool.query('UPDATE autoridad SET ? ',[newAutoridad]);
+    await pool.query('UPDATE autoridades SET ?',[newAutoridad]);
 });
+
+//Agregar una nueva autoridad
+router.post('/add',isLoggedIn,async(req,res,next)=>{
+    const {
+        idAutoridad,
+        dni,
+        telefono,
+        direccion,
+        nombre,
+        apellido,
+        fechaIngreso,
+        fechaNacimiento,
+        FichaMedica
+    } = req.body;
+
+    const newAutoridad ={
+        idAutoridad,
+        dni,
+        telefono,
+        direccion,
+        nombre,
+        apellido,
+        fechaIngreso,
+        fechaNacimiento,
+        FichaMedica
+    }
+
+    await pool.query('INSERT INTO autoridades SET= ?',[newAutoridad]);
+})
 
 module.exports = router;
