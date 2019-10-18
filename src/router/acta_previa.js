f638305c1c7fa8c7852e5d62c2ccea0e44cb79e4
AleaIactaEst <maikholsozaki@gmail.com>
2019-10-17 21:50:41 -0300

    Otros pequeños cambios

--- src/router/acta_previa.js
+++ src/router/acta_previa.js
@@ -44,8 +44,38 @@ ON AlumInscripcion_ProfActa_Previa.idActa = Materia_Acta_Previa.idActa`)
 router.get('/:idActaPrevia',async(req, res, next) =>
 {
     const {idActaPrevia} = req.body;
-    const arrayActaPrevia = await pool.query('SELECT * FROM Acta_Previa WHERE idActaPrevia = ?',[idActaPrevia]);
-    res.send(arrayActaPrevia);
+    const arrayActaPrevia = await pool.query( `
+    SELECT AlumInscripcion_ProfActa_Previa.*, Materia_Acta_Previa.idMateria, Materia_Acta_Previa.situacion, Materia_Acta_Previa.fechaCierre, Materia_Acta_Previa.turno, Materia_Acta_Previa.titulo
+    FROM (
+      SELECT Alumno_Inscripcion_Previa.*, NombreAutoridad, ApellidoAutoridad, Profe_Acta_Previa.dniAutoridad
+      FROM (
+        SELECT (Alumno.nombre) AS NombreAlumno, (Alumno.apellido) AS ApellidoAlumno, Inscripcion_Previa.*
+        FROM Alumno
+        INNER JOIN Inscripcion_Previa
+        ON Alumno.dniAlumno = Inscripcion_Previa.dniAlumno
+      ) AS Alumno_Inscripcion_Previa
+      INNER JOIN (
+        SELECT (Autoridades.nombre) AS NombreAutoridad, (Autoridades.apellido) AS ApellidoAutoridad, Profesores_Acta_Previa.*
+        FROM Autoridades
+        INNER JOIN Profesores_Acta_Previa
+        ON Autoridades.dniAutoridad = Profesores_Acta_Previa.dniAutoridad
+      ) AS Profe_Acta_Previa
+      ON Alumno_Inscripcion_Previa.idActa = Profe_Acta_Previa.idActa
+    ) AS AlumInscripcion_ProfActa_Previa
+    INNER JOIN (
+      SELECT Materia.titulo, Acta_Previa.*
+      FROM Materia
+      INNER JOIN Acta_Previa
+      ON Materia.idMateria = Acta_Previa.idMateria
+    ) AS Materia_Acta_Previa
+    ON AlumInscripcion_ProfActa_Previa.idActa = Materia_Acta_Previa.idActa
+    WHERE AlumInscripcion_ProfActa_Previa.idActa = ?`,[idActaPrevia])
+    .catch(err=>{return new Promise(()=>{
+      next(err)
+      })
+  });
+
+  res.json(idActaPrevia);
 });
 
 
